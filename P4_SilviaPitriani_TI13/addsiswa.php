eedb20926c37ff2b5028c929d13d9440d6a01837
silviapiw <114458991+silviapiw@users.noreply.github.com>
2023-03-31 15:23:44 +0700

    Praktikum 4

---
+++ P4_SilviaPitriani_TI13/addsiswa.php
@@ -0,0 +1,65 @@
+<?php   
+    include_once 'template/header.php'
+?>
+    <form>
+  <div class="form-group row">
+    <label for="text" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>
+          </div>
+        </div> 
+        <input id="text" name="text" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text1" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <input id="text1" name="text1" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text2" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <input id="text2" name="text2" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Radio Buttons</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="radio" id="radio_0" type="radio" class="custom-control-input" value="rabbit"> 
+        <label for="radio_0" class="custom-control-label">Rabbit</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="radio" id="radio_1" type="radio" class="custom-control-input" value="duck"> 
+        <label for="radio_1" class="custom-control-label">Duck</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="radio" id="radio_2" type="radio" class="custom-control-input" value="fish"> 
+        <label for="radio_2" class="custom-control-label">Fish</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="select" class="col-4 col-form-label">Select</label> 
+    <div class="col-8">
+      <select id="select" name="select" class="custom-select">
+        <option value="rabbit">Rabbit</option>
+        <option value="duck">Duck</option>
+        <option value="fish">Fish</option>
+      </select>
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+<?php
+    include_once 'template/footer.php'
+?>
